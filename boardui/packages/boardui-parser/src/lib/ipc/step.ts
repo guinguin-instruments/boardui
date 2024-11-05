a6231936f25fa3fd10c6a3132ae39bc1964eec0c
guinguin-instruments <guinguin.instruments@gmail.com>
2024-11-05 10:30:05 +0100

    Add needed nonstandardAttributes to Step.

--- boardui/packages/boardui-parser/src/lib/ipc/step.ts
+++ boardui/packages/boardui-parser/src/lib/ipc/step.ts
@@ -2,10 +2,12 @@ import { Component } from './component';
 import { Contour } from './contour';
 import { LayerFeature } from './layer-feature';
 import { Package } from './package';
+import { NonstandardAttribute } from './nonstandard-attribute';
 
 export class Step {
   name: string = null!;
   profile: Contour | null = null;
+  nonstandardAttributes: NonstandardAttribute[] = [];
   layerFeatures: LayerFeature[] = [];
   packages: Package[] = [];
   components: Component[] = [];
